1952155fedf7350f13a1646be7cc6ad8d47e8535
Ales Justin <ales.justin@gmail.com>
2012-01-28 22:29:59 +0100

    Fix MGSP. -- against new Weld API.

--- weld/src/main/java/org/jboss/as/weld/services/ModuleGroupSingletonProvider.java
+++ weld/src/main/java/org/jboss/as/weld/services/ModuleGroupSingletonProvider.java
@@ -67,7 +67,7 @@ public class ModuleGroupSingletonProvider extends SingletonProvider {
         // use Hashtable for concurrent access
         private final Map<ClassLoader, T> store = new Hashtable<ClassLoader, T>();
 
-        public T get() {
+        public T get(String id) {
             T instance = store.get(findParentModuleCl(getClassLoader()));
             if (instance == null) {
                 throw new IllegalStateException("Singleton not set for " + getClassLoader());
@@ -75,7 +75,7 @@ public class ModuleGroupSingletonProvider extends SingletonProvider {
             return instance;
         }
 
-        public void set(T object) {
+        public void set(String id, T object) {
             ClassLoader classLoader = getClassLoader();
             store.put(classLoader, object);
             if (deploymentClassLoaders.containsKey(classLoader)) {
@@ -85,7 +85,7 @@ public class ModuleGroupSingletonProvider extends SingletonProvider {
             }
         }
 
-        public void clear() {
+        public void clear(String id) {
             ClassLoader classLoader = getClassLoader();
             store.remove(classLoader);
             if (deploymentClassLoaders.containsKey(classLoader)) {
@@ -95,7 +95,7 @@ public class ModuleGroupSingletonProvider extends SingletonProvider {
             }
         }
 
-        public boolean isSet() {
+        public boolean isSet(String id) {
             return store.containsKey(findParentModuleCl(getClassLoader()));
         }
 
